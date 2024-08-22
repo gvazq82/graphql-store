f138a4ab979dfca040886450c9c6c9e550f09fbd
Gabriel Vazquez <gabrielvazquez@gabriels-mbp.lan>
2024-08-22 15:03:54 -0400

    Initial Graphql commit with local storage

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './data/schema';
+import resolvers from './data/resolvers';
+
+const PORT = 8080;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GrapQL is amazing');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true,
+}));
+
+app.listen(PORT, () => console.log(`Running server on localhost:${PORT}/graphql`));
